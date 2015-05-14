5ec6d2aac1c090e1ecb4e3788bc8a668a1c4608a
Rose THERESA <rosetheresa@Roses-MacBook-Air.local>
2015-05-13 20:45:03 -0700

    red and black checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 // Your JS goes here
+
+
+for (var i=0; i<81; i++) {
+	var newDiv = document.createElement("div");
+	newDiv.style.width = "11.1%";
+	newDiv.style.float = "left";
+	newDiv.style.paddingBottom = "11.1%";
+	newDiv.style.backgroundColor = "red";
+	if (i %2 === 0) newDiv.style.backgroundColor = "red";
+		else newDiv.style.backgroundColor = "black";
+	document.body.appendChild(newDiv);
+};
+
+for (var i=0; i<81; i++) {
+	var newDiv = document.createElement("div");
+	newDiv.style.width = "11.1%";
+	newDiv.style.float = "left";
+	newDiv.style.paddingBottom = "11.1%";
+	newDiv.style.backgroundColor ='#'+Math.floor(Math.random()*16777215).toString(16);
+	document.body.appendChild(newDiv);
+};
+var num = 150
+for (var i=0; i<81; i++) {
+	var newDiv = document.createElement("div");
+	newDiv.style.width = "11.1%";
+	newDiv.style.float = "left";
+	newDiv.style.paddingBottom = "11.1%";
+	newDiv.style.backgroundColor = "red";
+	if (i %2 === 0) {
+		newDiv.style.backgroundColor = "rgb(100,100,"+num+")";
+		num+=2;}
+	else {
+		newDiv.style.backgroundColor = "rgb("+num+",222,200)";
+		num+=2;
+	}
+
+	document.body.appendChild(newDiv);
+}
