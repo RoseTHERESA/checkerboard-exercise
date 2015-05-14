364358f6ae8f85985102278c2c557d441b06cb54
Rose THERESA <rosetheresa@Roses-MacBook-Air.local>
2015-05-13 20:56:22 -0700

    checkerboard with random colors

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 // Your JS goes here
-
-
 for (var i=0; i<81; i++) {
-	var newDiv = document.createElement("div");
-	newDiv.style.width = "11.1%";
-	newDiv.style.float = "left";
-	newDiv.style.paddingBottom = "11.1%";
-	newDiv.style.backgroundColor = "red";
-	if (i %2 === 0) newDiv.style.backgroundColor = "red";
-		else newDiv.style.backgroundColor = "black";
-	document.body.appendChild(newDiv);
-};
-
-/*for (var i=0; i<81; i++) {
 	var newDiv = document.createElement("div");
 	newDiv.style.width = "11.1%";
 	newDiv.style.float = "left";
@@ -20,6 +7,7 @@ for (var i=0; i<81; i++) {
 	newDiv.style.backgroundColor ='#'+Math.floor(Math.random()*16777215).toString(16);
 	document.body.appendChild(newDiv);
 };
+/*
 var num = 150
 for (var i=0; i<81; i++) {
 	var newDiv = document.createElement("div");
